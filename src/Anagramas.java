4cca341779ab186ee9c840fb6127033a309b38c0
Moacir Junior <skytec.dev@gmail.com>
2022-10-02 23:44:40 -0300

    classe anagramas 100% mas não passou em todos os testes

--- src/Anagramas.java
+++ src/Anagramas.java
@@ -5,26 +5,36 @@ import java.util.stream.IntStream;
 public class Anagramas {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-//        String a = scan.next();
-//        String b = scan.next();
-        String a = "anagram";
-        String b = "margana";
+        String a = scan.next();
+        String b = scan.next();
+//        String a = "anagram";
+//        String b = "margan";
         scan.close();
         boolean ret = isAnagram(a, b);
         System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
     }
 
-    private static boolean isAnagram(String a, String b) {
-
+    private static boolean isAnagram(String A, String B) {
+        String a = A.toLowerCase();
+        String b = B.toLowerCase();
         StringBuilder sb = new StringBuilder();
         a.chars().distinct().forEach(value -> sb.append((char) value));
-        System.out.println(sb.toString());
         List<Integer> f = new ArrayList<>();
+
         sb.toString().chars().forEach(value -> {
             f.add((int) a.chars().filter(c -> value == c).count());
         });
-        System.out.println(f.stream().toList());
-        return true;
+
+//        System.out.println(f.stream().toList());
+
+        List<Integer> g = new ArrayList<>();
+        sb.toString().chars().forEach(value -> {
+            g.add((int) b.chars().filter(c -> value == c).count());
+        });
+
+//        System.out.println(Arrays.equals(f.toArray(),g.toArray()));
+//        System.out.println(g.toString());
+        return Arrays.equals(f.toArray(),g.toArray());
 
     }
 }
